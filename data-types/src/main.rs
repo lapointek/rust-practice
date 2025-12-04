46d232a1a99f6d27163a46e774d3febc80098dbe
lapointek <lapointek.dev@gmail.com>
2025-12-04 10:24:34 -0500

    chore: add project practice code to data-types

--- data-types/src/main.rs
+++ data-types/src/main.rs
@@ -135,7 +135,7 @@ fn main() {
     /* let (name, age, department) = employee;
     println!("Name: {name}, age: {age}, Department: {department}");
     println!("{employee:#?}") */
-
+    /*
     let month_days: std::ops::Range<i32> = 1..31;
     println!("{month_days:?}");
     let month_days: std::ops::RangeInclusive<i32> = 1..=31;
@@ -155,5 +155,51 @@ fn main() {
 
     for color in colors {
         println!("{color} is a great color.");
-    }
+     */
+
+    /*
+    Declare an i32 variable assigned to 1337.
+    Use the underscore character to add a visual
+    separator between the numbers.
+
+    Cast the i32 to an i16 integer and assign the result
+    to a separate variable.
+
+    Declare a floating-point value of your choosing.
+    Print out the number with 3 digits of precision.
+
+    Declare a 'with_milk' variable set to a Boolean.
+    Declare a 'with_sugar` variable set to a Boolean.
+
+    Declare a 'is_my_type_of_coffee` variable. It should
+    be set to true if the coffee has both milk and sugar.
+
+    Declare an `is_acceptable_coffee` variable. It should
+    be set to true if the coffee has either milk or
+    sugar.
+
+    Declare an array with four i8 integers of your choosing
+    Print out the array in its Debug representation.
+
+    Declare a tuple consisting of the integer, float,
+    a Boolean, and the array that you previously declared.
+    Print out the tuple in its Debug representation.
+    */
+
+    let num: i32 = 13_37;
+    let num_i16 = num as i16;
+
+    let fnum = 18.1022;
+    println!("{:.3}", fnum);
+
+    let with_milk = true;
+    let with_sugar = true;
+    let is_my_type_of_cofee = with_milk && with_sugar;
+    let is_acceptable_coffee = with_milk || with_sugar;
+
+    let myarray: [i8; 4] = [10, 20, 30, 40];
+    dbg!(myarray);
+
+    let tup = (12, 20.30, true, myarray);
+    println!("{:#?}", tup);
 }
